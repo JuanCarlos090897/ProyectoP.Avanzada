dc08ae23914e78908f3c572a8f4756a003bae533
Juan Carlos <juanca_quesada@hotmail.com>
2018-02-19 00:32:41 -0600

    Proyecto 1.2
    Este proyecto trae implementado todo de bootstrap y jquery

---
+++ ProyectoPA/JavaScript/Registrarse.js
@@ -0,0 +1,33 @@
+// (function () {
+//     $('form').submit(function (event) {
+//         var mensaje = "";
+//         $('.validate').each(function () {
+//             if ($(this).val().trim().length < 5) {
+//                 var label = $('label', this.parentElement).text();
+//                 mensaje += "\n"+label.substring(0, label.length - 1);
+//             }
+//         });
+//         if (mensaje != "") {
+//             alert("Los siguiente campos tienen menos de 5 characteres:\n"+mensaje);
+//             event.preventDefault(event);
+//         }
+//     });
+// })();
+
+
+(function () {
+    $('form').submit(function (event) {
+        var mensaje = "";
+        $('.validate').each(function () {
+            if ($(this).val().trim().length < 5) {
+                var label = $('label', this.parentElement).text();
+                mensaje += "\n"+label.substring(0, label.length - 1);
+            }
+        });
+        if (mensaje != "") {
+            // alert("Los siguiente campos tienen menos de 5 characteres:\n"+mensaje);
+            // event.preventDefault(event);
+            alert("Los campos requieren mas de 5 campos:\n");
+        }
+    });
+})();

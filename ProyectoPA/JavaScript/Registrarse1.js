163c959b0c9d4f113d084f4400328b32389afddf
Juan Carlos <juanca_quesada@hotmail.com>
2018-02-22 21:35:41 -0600

    Proyecto 1.3
    22/02/2018

--- ProyectoPA/JavaScript/Registrarse1.js
+++ ProyectoPA/JavaScript/Registrarse1.js
@@ -1,13 +1,23 @@
-(function () {
+(function ValidarCampos () {
     $('form').submit(function (event) {
-        var mensaje = "";
-        $('.validate').each(function () {
-            if ($(this).val().trim().length < 5) {
+        var mensajetexto = "";
+        var mensajecedula="";
+        $('.validatetext').each(function () {
+            if ($(this).val().trim().length < 3) {
                 var label = $('label', this.parentElement).text();
-                mensaje += "\n"+label.substring(0, label.length - 1);
+                mensajetexto += "\n"+label.substring(0, label.length - 1);
             }
         });
-        if (mensaje != "") {
+        $('.validatecedula').each(function () {
+            if ($(this).val().trim().length < 9) {
+                var label = $('label', this.parentElement).text();
+                mensajecedula += "\n"+label.substring(0, label.length - 1);
+            }
+        });
+
+
+
+        if (mensajetexto != "" || mensajecedula ) {
             alert("Los siguiente campos tienen menos de 5 characteres:\n"+mensaje);
             event.preventDefault(event);
         }
